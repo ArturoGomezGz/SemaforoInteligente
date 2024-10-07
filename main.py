1a62d64b93a6d33bd99467184b6158e4ace75ca6
ArturoGomez <arturogomezgz04@gmail.com>
2024-10-07 10:34:50 -0600

    Pruebas con nuevo cascade

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 from clases.interseccion import Interseccion
 from clases.semaforo import Semaforo
 
-semaforo1 = Semaforo(idSemaforo = 1, ruta_video = "resources/video1.mp4")
-semaforo2 = Semaforo(idSemaforo = 2, ruta_video = "resources/video2.mp4")
+semaforo1 = Semaforo(idSemaforo = 1, ruta_video = "resources/video1.mp4", ruta_cascade="classifier/haarcascade_car.xml", scaleFactor=1.5, minNeighbors=9)
+semaforo2 = Semaforo(idSemaforo = 2, ruta_video = "resources/video2.mp4", ruta_cascade="classifier/haarcascade_car.xml")
 
 interseccion1 = Interseccion([semaforo1, semaforo2])
 
