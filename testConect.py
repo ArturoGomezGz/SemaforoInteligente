7e7de7d9952e97437d6aec0dc950f447d022151b
ArturoGomez <116232430+ArturoGomezGz@users.noreply.github.com>
2024-11-08 03:50:18 +0000

    Debuging

--- testConect.py
+++ testConect.py
@@ -13,25 +13,29 @@ conection = Conexion(baseDeDatos)
 print("mCiclos --------------------------------------")
 json_data = conection.getMCiclos()
 data = json.loads(json_data)
-print(data)
+for i in data:
+    print(i)
 print("")
 
 print("dCiclos --------------------------------------")
 json_data = conection.getDCiclos()
 data = json.loads(json_data)
-print(data)
+for i in data:
+    print(i)
 print("")
 
 print("Semaforos --------------------------------------")
 json_data = conection.getSemaforos()
 data = json.loads(json_data)
-print(data)
+for i in data:
+    print(i)
 print("")
 
 print("Interseciones --------------------------------------")
 json_data = conection.getIntersecciones()
 data = json.loads(json_data)
-print(data)
+for i in data:
+    print(i)
 print("")
 
 conection.cerrarConexion()
