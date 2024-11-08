27f6bd4890950e7b672b2b64db5c8205c92c013d
ArturoGomez <116232430+ArturoGomezGz@users.noreply.github.com>
2024-11-08 04:22:57 +0000

    Debuging

---
+++ test.py
@@ -0,0 +1,21 @@
+from conexion.conexion import Conexion
+import json
+import time
+import os
+
+baseDeDatos = {
+    "server" : "localhost",  # Cambia esto a tu servidor SQL
+    "database" : "SemaforoInteligente",  # Cambia esto a tu base de datos
+    "usuario" : "arturo",
+    "contrasena" : "Pword1",
+}
+
+conection = Conexion(baseDeDatos)
+
+conection.getUltimoRegistro(1)
+
+conection.cerrarConexion()
+
+
+
+
