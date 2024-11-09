2ac844bd368f45bce7e8bdb2e03b6a7ff7e55cdd
ArturoGomez <116232430+ArturoGomezGz@users.noreply.github.com>
2024-11-09 01:51:16 +0000

    creacion de login.js

---
+++ webSite/scripts/login.js
@@ -0,0 +1,17 @@
+// Nombre de usuario que deseas buscar
+const usuario = 'testing';
+
+// Realiza una solicitud GET a la API
+fetch(`/getUsuario/${usuario}`)
+  .then(response => {
+    if (!response.ok) {
+      throw new Error('Usuario no encontrado');
+    }
+    return response.json(); // Convierte la respuesta a JSON
+  })
+  .then(data => {
+    console.log('Datos del usuario:', data); // Imprime los datos en la consola o úsalos en tu aplicación
+  })
+  .catch(error => {
+    console.error('Error:', error); // Muestra el error si ocurre algún problema
+  });
