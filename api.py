7ea141ba40f8c9ebed98d458f27134ef95241a9d
ArturoGomez <116232430+ArturoGomezGz@users.noreply.github.com>
2024-11-09 01:20:01 +0000

    Metodo para actualizar tCiclo agregado

--- api.py
+++ api.py
@@ -78,7 +78,7 @@ def updateTCiclo(interseccion_id,tCiclo):
     conexion = Conexion(baseDeDatos)
     result = conexion.updateInterseccionTime(interseccion_id,tCiclo)
     conexion.cerrarConexion()
-    return jsonify(result)
+    return jsonify({"Modificacion exitosa"})
 
 if __name__ == '__main__':
     app.run(debug=True)
