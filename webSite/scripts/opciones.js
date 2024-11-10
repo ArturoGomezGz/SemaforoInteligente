cedb4d5edb82eb592af918a1fb4a8641b6f388f6
ArturoGomez <116232430+ArturoGomezGz@users.noreply.github.com>
2024-11-10 04:47:19 +0000

    Ajustes a pagina web

--- webSite/scripts/opciones.js
+++ webSite/scripts/opciones.js
@@ -1,24 +1,21 @@
-function agregarOpciones(){
-    const select = document.getElementById("interseccion");
-    let options 
+const select = document.getElementById("interseccion");
+let options;
 
-    const url = 'http://127.0.0.1:5000/intersecciones'
-    axios.get(url)
-        .then(response => {
-            // Imprimir los datos de la respuesta
-            options = JSON.parse(response.data);
-            console.log(options)
-            options.forEach(optionData => {
-                // Crea un elemento <option>
-                const option = document.createElement("option");
-                option.value = 'Interseccion '+optionData.id;
-                
-                select.appendChild(option);
-        })
-        .catch(error => {
-            // Manejo de errores
-            console.error("Error fetching data:", error);
+const url = 'http://127.0.0.1:5000/intersecciones';
+axios.get(url)
+    .then(response => {
+        // Parse response data
+        options = response.data;
+        console.log(options);
+        options.forEach(optionData => {
+            // Create an <option> element
+            const option = document.createElement("option");
+            option.value = 'Interseccion ' + optionData.id;
+            option.text = optionData.name;  // Adjust this to match the property you want as text
+            select.appendChild(option);
         });
+    })
+    .catch(error => {
+        // Error handling
+        console.error("Error fetching data:", error);
     });
-}
-agregarOpciones()
