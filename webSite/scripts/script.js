c805bc15da9b9caeeb74eca4618149de4c08afa2
ArturoGomez <116232430+ArturoGomezGz@users.noreply.github.com>
2024-11-10 18:44:09 +0000

    ajustes a script de grafico

--- webSite/scripts/script.js
+++ webSite/scripts/script.js
@@ -76,31 +76,6 @@ function verEstadisticas(){
                 }
             }
         });
-
-        const ctx3 = document.getElementById('grafico3').getContext('2d');
-        const grafico3 = new Chart(ctx3, {
-            type: 'pie',
-            data: {
-                labels: ['Comida', 'Transporte', 'Entretenimiento', 'Otros'],
-                datasets: [{
-                    label: 'Categorías de Gastos',
-                    data: [300, 50, 100, 40],
-                    backgroundColor: [
-                        'rgba(255, 206, 86, 0.2)',
-                        'rgba(54, 162, 235, 0.2)',
-                        'rgba(255, 99, 132, 0.2)',
-                        'rgba(75, 192, 192, 0.2)'
-                    ],
-                    borderColor: [
-                        'rgba(255, 206, 86, 1)',
-                        'rgba(54, 162, 235, 1)',
-                        'rgba(255, 99, 132, 1)',
-                        'rgba(75, 192, 192, 1)'
-                    ],
-                    borderWidth: 1
-                }]
-            }
-        });
     })
     .catch(error => {
         // Manejo de errores
