16d1eb5e23c4bd91410359e40a7a07da7ef08a7c
ArturoGomez <arturogomezgz04@gmail.com>
2024-11-14 13:25:22 -0600

    Ajustes

--- webSite/scripts/script.js
+++ webSite/scripts/script.js
@@ -64,6 +64,7 @@ function iniciarSesion(){
   
     // URL local
     let url = 'http://127.0.0.1:5000/getUsuario/'+usuario;
+    console.log(url)
   
     axios.get(url).then(response => {
         // Imprimir los datos de la respuesta
