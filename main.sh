71b347d45e678d0a36f7878d85570cc5de276395
ArturoGomez <arturogomezgz04@gmail.com>
2024-11-14 19:24:02 -0600

    Pruebas

---
+++ main.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+# Rutas de los archivos de Python
+script="main.py"
+
+# Número de veces que deseas ejecutar los archivos
+iterations=10  # Cambia este número según tus necesidades
+
+# Bucle para ejecutar los scripts n veces
+for ((i=1; i<=iterations; i++))
+do
+    echo "Ejecución $i de $iterations"
+    
+    # Ejecutar el primer script
+    cd "simulacion"
+    echo "Ejecutando $script..."
+    python "$script"
+    cd ".."
+
+    # Ejecutar el segundo script
+    cd "procesamiento"
+    echo "Ejecutando $script..."
+    python "$script"
+    cd ".."
+done
+
+echo "Ejecución completa."
