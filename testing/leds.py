b075883bd749897ff250a0dbf40bf9b973047321
ArturoGomez <arturogomezgz04@gmail.com>
2024-11-26 21:11:39 -0600

    Pruebas

--- testing/leds.py
+++ testing/leds.py
@@ -38,3 +38,13 @@ print("Interseccion 1 creada")
 
 semaforo1.encender_rojo()
 semaforo2.encender_rojo()
+
+time.sleep(3)
+
+semaforo1.encender_amarillo()
+semaforo2.encender_amarillo()
+
+time.sleep(3)
+
+semaforo1.encender_verde()
+semaforo2.encender_verde()
