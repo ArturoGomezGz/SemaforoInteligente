ff8644411ae46330a09a56cd953800db55226c52
ArturoGomez <arturogomezgz04@gmail.com>
2024-11-27 13:49:47 -0600

    Implementacion de clase heredad a semaforoSimulacion

--- simulacion/main.py
+++ simulacion/main.py
@@ -1,18 +1,20 @@
 import sys
 sys.path.append(r"../")
 from clases.interseccion import Interseccion
-from clases.semaforo import Semaforo
+from clases.semaforoSimulacion import SemaforoSimulacion
 import json
 import os
 import requests
 
-semaforo1 = Semaforo(
+semaforo1 = SemaforoSimulacion(
     json.loads(requests.get("http://127.0.0.1:5000/semaforo/1").json())[0],
+    "resources/video1.mp4"
     )
 print("Semaforo 1 creado")
 
-semaforo2 = Semaforo(
+semaforo2 = SemaforoSimulacion(
     json.loads(requests.get("http://127.0.0.1:5000/semaforo/2").json())[0],
+    "resources/video2.mp4"
     )
 print("Semaforo 2 creado")
 
@@ -27,5 +29,3 @@ semaforo2.imprime()
 
 semaforo1.recortarVideo()
 semaforo2.recortarVideo()
-
-conection.cerrarConexion()
