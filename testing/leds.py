fd689f583df4aca04220feacf206184c70bff0e0
ArturoGomez <arturogomezgz04@gmail.com>
2024-11-26 21:39:53 -0600

    Pruebas

--- testing/leds.py
+++ testing/leds.py
@@ -7,9 +7,9 @@ import requests
 import time
 
 pinesS1 = {
-    "redPin": 2,
-    "greenPin": 3,
-    "yellowPin": 4
+    "redPin": 3,
+    "greenPin": 5,
+    "yellowPin": 7
 }
 """ pinesS2 = {
     "redPin": 18,
